fb04e2c402eb5046a56203b54d010f204d3f01ec
Nicole Cantrell <nikkicantrell@Nicoles-MacBook-Air.local>
2017-05-24 22:06:56 -0400

    Adds ability to quit at any time

--- Calculator.py
+++ Calculator.py
@@ -5,6 +5,7 @@ class Calculator(object):
     def __init__(self):
         pass
     def calculate(self, gradesAndCredits):
+        """Calculates the users GPA"""
         qualityPts = 0
         numCredits = 0
         for x in range(len(gradesAndCredits)):
@@ -26,15 +27,27 @@ if __name__ == "__main__":
     if len(sys.argv) < 1:
         print ("Usage: python calculator.py")
     else:
-        numClasses = raw_input("How many classes do you have? ")
+        print('Please note you can exit the program at any time by entering "quit"')
+        while True:
+            try:
+                numClasses = raw_input("How many classes do you have? ")
+                numClassesInt = int(numClasses)
+            except ValueError:
+                if numClasses == "quit":
+                    sys.exit()
+                print("Sorry your answer is not valid. Please enter a number.")
+            else:
+                break
         gradesAndCredits = []
-        for x in range(int(numClasses)):
+        for x in range(numClassesInt):
             while True:
                 try:
                     credits = raw_input("How many credits is class #" +
                                         str(x+1) + "? ")
                     creditsInt = int(credits)
                 except ValueError:
+                    if credits == "quit":
+                        sys.exit()
                     print("Sorry your answer is not valid. Please enter a number.")
                 else:
                     break
@@ -45,7 +58,10 @@ if __name__ == "__main__":
                 if (gradeStripped != 'A' and gradeStripped != 'B'
                     and gradeStripped != 'C' and gradeStripped != 'D'
                     and gradeStripped != 'F'):
-                    print("Sorry your answer is not valid. Please enter one " +
+                    if(gradeStripped == "quit"):
+                        sys.exit()
+                    else:
+                        print("Sorry your answer is not valid. Please enter one " +
                           "of these options: A, B, C, D, F)")
                 else:
                     break
